4a97ed973867b79d5ab8d7d3a99e1f2f547e1b3f
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2016-02-08 11:22:28 +0200

    Render dependency graph
    After the dependency graph has been internally created, generate a
    .dot file representing the graph and call graphviz to render it.
    Transitive reduction is applied to the graph definition.

--- lib/isafw/isaplugins/ISA_dep_plugin.py
+++ lib/isafw/isaplugins/ISA_dep_plugin.py
@@ -1,4 +1,7 @@
 import os
+import textwrap
+import subprocess
+
 try:
     import cPickle as pickle
 except ImportError:
@@ -6,6 +9,7 @@ except ImportError:
 
 log = '/isafw_deplog'
 temp_file = '/isafw_dep_tmp'
+report_file = '/dep_report'
 DEPChecker = None
 
 
@@ -13,10 +17,12 @@ class ISA_DEPChecker:
     initialized = False
 
     def __init__(self, ISA_config):
+        self.depgraph = {}
         self.logdir = ISA_config.logdir
         self.reportdir = ISA_config.reportdir
-        self.initialized = True
+        self.timestamp = ISA_config.timestamp
 
+        self.initialized = True
         with open(self.logdir + log, 'a') as flog:
             flog.write("\nPlugin ISA_DEPChecker initialized!\n")
 
@@ -30,14 +36,13 @@ class ISA_DEPChecker:
     def process_report(self):
         tmp = open(self.reportdir + temp_file, 'rb')
 
-        depgraph = {}
         while True:
             try:
                 pkg, deps = pickle.load(tmp)
 
                 # update dependencies if node already exists in depgraph,
                 # add new empty node otherwise
-                node = depgraph.setdefault(pkg, set())
+                node = self.depgraph.setdefault(pkg, set())
                 node.update(deps)
 
             except EOFError:
@@ -50,9 +55,45 @@ class ISA_DEPChecker:
         except OSError:
             pass
 
+        dot_graph = self.generate_dot()
+        report_path = self.reportdir + report_file + '_' + self.timestamp + '.dot'
+        with open(report_path, 'w') as f:
+            f.write(dot_graph)
+
+        # render the graph
+        rc = subprocess.call(['which', 'dot'])
+        if rc == 0:
+            # remove transient redundancy from graph before rendering
+            ps = subprocess.Popen(('tred', report_path), stdout=subprocess.PIPE)
+            subprocess.call(
+                    ('dot', '-Tpng', '-o', report_path[:-3] + 'png'),
+                    stdin=ps.stdout
+            )
+            ps.wait()
+        else:
+            with open(self.logdir + log, 'a') as flog:
+                flog.write('Graphviz is missing, the graph will not be rendered.\n')
+
         # put dependency graph in log file. todo: remove me
         with open(self.logdir + log, 'a') as flog:
-            flog.write(str(depgraph))
+            flog.write(str(self.depgraph))
+
+    def generate_dot(self):
+        """ Generate a digraph definition in the DOT language """
+        graph_template = textwrap.dedent('''\
+        digraph dependency_graph {{
+        \tranksep=3;
+        {edges}
+        }}
+        ''')
+        edge_template = '\t"{a}" -> "{b}";'
+
+        edges = []
+        for node, deps in self.depgraph.iteritems():
+            for dep in deps:
+                edges.append(edge_template.format(a=node, b=dep))
+
+        return graph_template.format(edges='\n'.join(edges))
 
 
 # ======== supported callbacks from ISA ============= #
